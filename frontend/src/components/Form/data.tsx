dc9a1ff316edca62dd5f0f7ea06d328e6f6d15aa
nftchance <chance@utc24.io>
2023-04-18 11:29:32 -0500

    fix: deprecate v1 architecture

--- frontend/src/components/Form/data.tsx
+++
@@ -1,7 +0,0 @@
-const tooltips = {
-    size: 'How many contacts do you want to get?',
-    priorities: 'Decide what is most important to you and get contacts that have been filtered to fit.',
-    referrer: 'Save 60% by entering a referral code!',
-}
-
-export { tooltips }
