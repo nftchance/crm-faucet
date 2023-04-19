b51b7ad19c5e2029826a99ae949d31fd1eb7d949
nftchance <chance@utc24.io>
2023-04-19 00:06:54 -0500

    feat: ens integration

--- source/jobs.py
+++ source/jobs.py
@@ -6,18 +6,33 @@ from utils.jobs import Job, JobManager
 from utils.queries import ENS
 from utils.shroom import QUERY
 
+from .models import Source, SourceIdentifier
+
 @util.close_old_connections
 def ens() -> None:
-    address_str = ",".join([f"lower('{address}')" for address in ENS.addresses])
-
-    response = QUERY(ENS.format(address_str))
+    addresses = list(Source.objects.active().values_list("address", flat=True))
+    response = QUERY(ENS.format(",".join([f"lower('{address}')" for address in addresses])))
 
     if not response:
         return
-    
-    identifiers = {record["address"] for record in response.records}
 
-    print(identifiers)
+    sources = {source.address: source for source in Source.objects.filter(address__in=[record.get('address') for record in response.records])}
+    identifiers: List[SourceIdentifier] = []
+
+    if isinstance(response.records, list):
+        response.records = {record.get('address'): record for record in response.records}
+
+    for record in response.records.values():
+        address = record.get('address')
+        source = sources.get(address)
+
+        if source:
+            for column, value in record.items():
+                if column != 'address' and value:
+                    if not SourceIdentifier.objects.filter(source=source, source_type=column).exists():
+                        identifiers.append(SourceIdentifier(source=source, source_type=column, value=value))
+
+    SourceIdentifier.objects.bulk_create(identifiers)
 
 jobs: List[Job] = [
     Job("ens", ens, trigger="*/59 * * * *"),
--- source/models.py
+++ source/models.py
@@ -1,29 +1,5 @@
 from django.db import models
 
-class SourceIdentifier(models.Model):
-    SOURCE_TYPES = (
-        ("twitter_id", "Twitter ID"),
-        ("twitter_username", "Twitter Username"),
-        ("twitter_handle", "Twitter Handle"),
-        ("github_username", "Github Username"),
-        ("ens_name", "ENS Name"),
-        ("linkedin_username", "LinkedIn Username"),
-        ("discord_username", "Discord Username"),
-        ("reddit_username", "Reddit Username"),
-        ("telegram_username", "Telegram Username"),
-        ("email", "Email"),
-        ("lens_handle", "Lens Handle"),
-    )
-
-    source_type = models.CharField(max_length=255, choices=SOURCE_TYPES)
-    value = models.CharField(max_length=255, blank=True, null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self) -> str:
-        return f"{self.source_type}:{self.identifier}"
-
 class SourceQuerySet(models.QuerySet):
     def with_identifiers(self):
         return self.prefetch_related("identifiers")
@@ -51,7 +27,6 @@ class Source(models.Model):
     is_active = models.BooleanField(default=True)
 
     address = models.CharField(max_length=256, blank=False, null=False)
-    identifiers = models.ManyToManyField(SourceIdentifier, blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -63,3 +38,29 @@ class Source(models.Model):
 
     class Meta:
         ordering = ["-created"]
+
+class SourceIdentifier(models.Model):
+    SOURCE_TYPES = (
+        ("twitter_id", "Twitter ID"),
+        ("twitter_username", "Twitter Username"),
+        ("twitter_handle", "Twitter Handle"),
+        ("github_username", "Github Username"),
+        ("ens_name", "ENS Name"),
+        ("linkedin_username", "LinkedIn Username"),
+        ("discord_username", "Discord Username"),
+        ("reddit_username", "Reddit Username"),
+        ("telegram_username", "Telegram Username"),
+        ("email", "Email"),
+        ("lens_handle", "Lens Handle"),
+    )
+
+    source = models.ForeignKey(Source, on_delete=models.CASCADE, related_name="identifiers")
+
+    source_type = models.CharField(max_length=255, choices=SOURCE_TYPES)
+    value = models.CharField(max_length=255, blank=True, null=True)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.source_type}:{self.identifier}"
