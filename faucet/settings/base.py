c25500a708c94bfd0f86086801fce27f7c7e1a5f
nftchance <chance@utc24.io>
2023-04-19 17:56:00 -0500

    fix: local

--- faucet/settings/base.py
+++ faucet/settings/base.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.management.utils import get_random_secret_key
 
-LOCAL = os.getenv("LOCAL", False)
+LOCAL = os.getenv("LOCAL", True)
 
 DEBUG = os.getenv("DEBUG", True)
 
