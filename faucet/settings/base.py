dc13fb66ed6b336c4bca104f584ad5a4d49bbd1d
nftchance <chance@utc24.io>
2023-04-19 17:42:08 -0500

    fix: false local by default

--- faucet/settings/base.py
+++ faucet/settings/base.py
@@ -1,7 +1,7 @@
-import os 
+import os
 
 from django.core.management.utils import get_random_secret_key
 
-LOCAL = os.getenv('LOCAL', True)
+LOCAL = os.getenv("LOCAL", False)
 
 SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
